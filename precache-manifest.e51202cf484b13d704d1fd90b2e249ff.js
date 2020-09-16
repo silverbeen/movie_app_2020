24d4faa8c564720700ca971c4793b86c09a1d9bc
강은빈 <kub0803@naver.com>
2020-09-16 22:54:02 +0900

    Updates

---
+++ precache-manifest.e51202cf484b13d704d1fd90b2e249ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4141a1dee8e5edf9bf22b93ff013e380",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2d2d3e31f28b94874211",
+    "url": "/movie_app_2020/static/css/main.cb9aa20c.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2d3e31f28b94874211",
+    "url": "/movie_app_2020/static/js/main.76e00879.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
